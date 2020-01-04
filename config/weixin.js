8049f16adbe82c393c0fa9ca21cfa8ecbc113ea2
tongxing <tongxing@huami.com>
2020-01-04 11:45:48 +0800

    Revert "feat | > 更改监听端口为3000"
    This reverts commit c3ea868d5be014b1557a56333c7019886bd2963b.

--- config/weixin.js
+++ config/weixin.js
@@ -2,5 +2,5 @@ module.exports.weixin = {
   // 微信公众号相关设置
   appid: 'wxb19af9a3b6b3941d',
   secret: 'b8c6e3db8828ea9c9c9f823e508bdd00',
-  redirectUri: 'http://47.105.153.8:3000/home/resolve'
+  redirectUri: 'http://52.199.44.119:8000/home/resolve'
 };
